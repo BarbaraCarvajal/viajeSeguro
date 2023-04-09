30c46fb2a6a7068de93d82b164ae9d652f3e8b72
Barbara Carvajal <barb.carvajalsaez@gmail.com>
2023-04-08 22:18:59 -0400

    aaa

--- app/src/main/java/com/example/viajeseguro/MainActivity.kt
+++ app/src/main/java/com/example/viajeseguro/MainActivity.kt
@@ -2,10 +2,72 @@ package com.example.viajeseguro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navegacion : BottomNavigationView
+    private val mONavMenu = BottomNavigationView.OnNavigationItemSelectedListener{
+        item ->
+            when(item.itemId){
+                R.id.itemFragment1 ->{
+                    supportFragmentManager.commit {
+
+                        replace<PrimerFragment>(R.id.frameConteiner)
+                        setReorderingAllowed(true)
+                        addToBackStack("replacement")
+
+                    }
+                    return@OnNavigationItemSelectedListener  true
+                }
+                R.id.itemFragment2 ->{
+                    supportFragmentManager.commit {
+
+                        replace<SegundoFragment>(R.id.frameConteiner)
+                        setReorderingAllowed(true)
+                        addToBackStack("replacement")
+
+                    }
+                    return@OnNavigationItemSelectedListener  true
+                }
+                R.id.itemFragment3 ->{
+                    supportFragmentManager.commit {
+
+                        replace<TercerFragment>(R.id.frameConteiner)
+                        setReorderingAllowed(true)
+                        addToBackStack("replacement")
+
+                    }
+                    return@OnNavigationItemSelectedListener  true
+                }
+            }
+
+
+
+
+
+        false
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        navegacion = findViewById(R.id.navMenu)
+        navegacion.setOnNavigationItemSelectedListener(mONavMenu)
+
+
+        supportFragmentManager.commit {
+
+            replace<PrimerFragment>(R.id.frameConteiner)
+            setReorderingAllowed(true)
+            addToBackStack("replacement")
+
+        }
+
     }
 }
